692ce18d7fd1ada7277b00eff992cf8356b14304
Allyson Athyrson <athyrsonallyson@gmail.com>
2020-01-05 12:49:37 -0300

    pesquisando por outros usuários

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -29,6 +29,24 @@
 
                 header('Location: /timeline');
         }
+        public function quemSeguir(){
+            $this->validaAutenticacao();
+
+            $pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+
+            $usuarios = array();
+
+            if($pesquisarPor != ''){
+                $usuario = Container::getModel('Usuario');
+                $usuario->__set('nome', $pesquisarPor);
+                $usuarios = $usuario->getAll();
+            }
+
+            $this->view->usuarios = $usuarios;
+            
+            $this->render('quemSeguir');
+        }
+
         public function validaAutenticacao(){
             session_start();
 
