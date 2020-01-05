f71567a3cf6bfb83009f2e4df4a2e5fc8ab94f23
Allyson Athyrson <athyrsonallyson@gmail.com>
2020-01-05 14:31:14 -0300

    remove proprio usuario da pesquisa

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -39,6 +39,7 @@
             if($pesquisarPor != ''){
                 $usuario = Container::getModel('Usuario');
                 $usuario->__set('nome', $pesquisarPor);
+                $usuario->__set('id', $_SESSION['id']);
                 $usuarios = $usuario->getAll();
             }
 
